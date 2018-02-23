ffb3eca40086b06468db83504a81c18fd87c724f
Dane Avilla <github.com@avilla.net>
2018-02-22 21:45:26 -0800

    Updating the shell so the commands and orientation values would be uppercase.

--- ruby_robot/lib/ruby_robot/shell.rb
+++ ruby_robot/lib/ruby_robot/shell.rb
@@ -13,7 +13,7 @@ class Shell < ::Bombshell::Environment
   #
   # Place a robot
   #
-  def place(x, y, direction)
+  def PLACE(x, y, direction)
     # Save state in case place is called w/ invalid coords
     orig_robot = @robot
     orig_tabletop = @tabletop
@@ -29,30 +29,162 @@ class Shell < ::Bombshell::Environment
       STDERR.puts $!
     end
   end
-  # alias :PLACE :place
 
-  def move
+  def MOVE
     return if @robot.nil?
     @robot.move
   end
-  # alias :MOVE :move 
 
-  def left
+  def LEFT
     return if @robot.nil?
     @robot.left
   end
-  # alias :LEFT :left 
 
-  def right
+  def RIGHT
     return if @robot.nil?
     @robot.right
   end
-  # alias :RIGHT :right
 
-  def report
+  def REPORT
     return if @robot.nil?
     STDOUT.puts @robot.report
   end
-  # alias :REPORT :report
+
+  # Exit Bombshell
+  alias :QUIT   :quit
 end
 end
+
+#
+# Monkeypatch Irb to allow no-arg uppercase methods
+#
+module IRB
+class Irb
+    # Evaluates input for this session.
+    def eval_input
+      @scanner.set_prompt do
+        |ltype, indent, continue, line_no|
+        if ltype
+          f = @context.prompt_s
+        elsif continue
+          f = @context.prompt_c
+        elsif indent > 0
+          f = @context.prompt_n
+        else
+          f = @context.prompt_i
+        end
+        f = "" unless f
+        if @context.prompting?
+          @context.io.prompt = p = prompt(f, ltype, indent, line_no)
+        else
+          @context.io.prompt = p = ""
+        end
+        if @context.auto_indent_mode
+          unless ltype
+            ind = prompt(@context.prompt_i, ltype, indent, line_no)[/.*\z/].size +
+              indent * 2 - p.size
+            ind += 2 if continue
+            @context.io.prompt = p + " " * ind if ind > 0
+          end
+        end
+      end
+
+      @scanner.set_input(@context.io) do
+        signal_status(:IN_INPUT) do
+          if l = @context.io.gets
+            #
+            # Begin monkeypatch: turn uppercase constants into
+            # method calls
+            #
+            if [:MOVE,:LEFT,:RIGHT,:REPORT,:QUIT].include?(l.strip.to_sym)
+              l = "#{l.strip}()\n"
+            end
+            #
+            # End monkeypatch
+            #
+            print l if @context.verbose?
+          else
+            if @context.ignore_eof? and @context.io.readable_after_eof?
+              l = "\n"
+              if @context.verbose?
+                printf "Use \"exit\" to leave %s\n", @context.ap_name
+              end
+            else
+              print "\n"
+            end
+          end
+          l
+        end
+      end
+
+      @scanner.each_top_level_statement do |line, line_no|
+        signal_status(:IN_EVAL) do
+          begin
+            line.untaint
+            @context.evaluate(line, line_no)
+            output_value if @context.echo?
+            exc = nil
+          rescue Interrupt => exc
+          rescue SystemExit, SignalException
+            raise
+          rescue Exception => exc
+          end
+          if exc
+            print exc.class, ": ", exc, "\n"
+            if exc.backtrace && exc.backtrace[0] =~ /irb(2)?(\/.*|-.*|\.rb)?:/ && exc.class.to_s !~ /^IRB/ &&
+                !(SyntaxError === exc)
+              irb_bug = true
+            else
+              irb_bug = false
+            end
+
+            messages = []
+            lasts = []
+            levels = 0
+            if exc.backtrace
+              for m in exc.backtrace
+                m = @context.workspace.filter_backtrace(m) unless irb_bug
+                if m
+                  if messages.size < @context.back_trace_limit
+                    messages.push "\tfrom "+m
+                  else
+                    lasts.push "\tfrom "+m
+                    if lasts.size > @context.back_trace_limit
+                      lasts.shift
+                      levels += 1
+                    end
+                  end
+                end
+              end
+            end
+            print messages.join("\n"), "\n"
+            unless lasts.empty?
+              printf "... %d levels...\n", levels if levels > 0
+              print lasts.join("\n"), "\n"
+            end
+            print "Maybe IRB bug!\n" if irb_bug
+          end
+          if $SAFE > 2
+            abort "Error: irb does not work for $SAFE level higher than 2"
+          end
+        end
+      end
+    end
+end
+end
+
+#
+# Map these constants to symbols so the shell
+# will pass them through
+#
+module Bombshell
+module Shell
+  NORTH=:north
+  SOUTH=:south
+  EAST=:east
+  WEST=:west
+end
+end
+# Tell shell not to show lower-case 'quit' method; it is aliased
+# to #QUIT along w/ all the other upper-case methods.
+::Bombshell::Shell::Commands::HIDE << :quit
